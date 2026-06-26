60b27de2028868370046ef7303390bc696e5fe5f
Leo Siu <leonardoy.siu@gmail.com>
2026-06-26 01:07:04 -0700

    init: add macOS app scaffold

---
+++ apps/Niblet/niblet/niblet/nibletApp.swift
@@ -0,0 +1,17 @@
+//
+//  nibletApp.swift
+//  niblet
+//
+//  Created by Leonardo Siu on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct nibletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/Niblet/niblet/nibletTests/nibletTests.swift
@@ -0,0 +1,19 @@
+//
+//  nibletTests.swift
+//  nibletTests
+//
+//  Created by Leonardo Siu on 6/26/26.
+//
+
+import Testing
+@testable import niblet
+
+struct nibletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
