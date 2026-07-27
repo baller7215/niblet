54545a610c788815b618a6a365281ec768982aab
Leo Siu <leonardoy.siu@gmail.com>
2026-07-27 00:13:08 -0700

    fix + feat(#8): add Combine import + connect app state to nibletApp

--- apps/Niblet/niblet/niblet/nibletApp.swift
+++ apps/Niblet/niblet/niblet/nibletApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct nibletApp: App {
+    @StateObject private var appState = NibletAppState()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appState)
         }
     }
 }
